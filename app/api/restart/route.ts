9dc130766174e81c9bf5402a4ba3056f3b9b737c
Luca <lucavallazza@gmail.com>
2023-11-18 20:16:34 -0300

    Types import error

--- app/api/restart/route.ts
+++ app/api/restart/route.ts
@@ -2,7 +2,14 @@ import prisma from "@/app/db";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    await prisma.users.deleteMany()
-    await prisma.character.deleteMany()
-    return new NextResponse('Reseteado', {status:200})
+    try{
+        await prisma.users.deleteMany()
+        await prisma.character.deleteMany()
+    
+        return new NextResponse('Reseteado', {status:200})
+
+    }catch(e){
+        
+        return new NextResponse('Something went wrong...', {status:500})
+    }
 }
